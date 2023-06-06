ba32dc910c05b8e843fe2be6d904a7d10357c7d5
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-06-06 10:23:08 -0300

    se agregan dependences

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
 
 function App() {
 
   return (
     <>
+    <Container id="containerPrincipal">
+    </Container>
+    <footer className='bg-dark text-light text-center py-3'>
+      <p>&copy; Todos los derechos reservados</p>
+    </footer>
     </>
   )
 }
