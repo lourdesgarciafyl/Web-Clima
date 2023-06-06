b224452e43b893456c4497841eca019caa126e45
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-06-06 11:24:20 -0300

    se agregan validaciones con react hook form

--- src/components/FormularioPais.jsx
+++ src/components/FormularioPais.jsx
@@ -1,14 +1,58 @@
-import { Form } from "react-bootstrap";
+import { Button, Form, Card } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
 
 const FormularioPais = () =>{
+    const [clima, setClima] = useState()
 
+    const { register, handleSubmit, formState: { errors }, } = useForm();
+
+    const apretarBuscar = () => {
+
+    }
     return(
-        <section>
-            <div>
-
-            </div>
+        <section className="my-4">
+            <h1 className="display-5 text-center">Consulta el clima</h1>
+            <Card className="container-wrapper">
+                <Card.Body>
+                    <Form onSubmit={handleSubmit(apretarBuscar)}>
+                        <Form.Group className="mb-4" controlId="controlCiudad">
+                            <Form.Label className="lead">Ingrese una ciudad</Form.Label>
+                            <Form.Control 
+                            aria-label="Ejemplo: San Miguel de Tucumán"
+                            type="text"
+                            placeholder="Ejemplo: San Miguel de Tucumán"
+                            {...register("ciudad", {required: "Campo obligatorio"})}
+                            >
+                            </Form.Control>
+                            <Form.Text className="text-danger">{errors.ciudad?.message}</Form.Text>
+                        </Form.Group>
+                        <Form.Group className="mb-4" controlId="controlPais">
+                            <Form.Label className="lead">Seleccione un país</Form.Label>
+                            <Form.Select 
+                            aria-label="Ejemplo: San Miguel de Tucumán"
+                            type="text"
+                            placeholder="Ejemplo: San Miguel de Tucumán"
+                            {...register("pais", {required: "Campo obligatorio"})}
+                            >
+                                                <option value="">--Seleccione un pais--</option>
+                                                <option value="AR">Argentina</option>
+                                                <option value="CL">Chile</option>
+                                                <option value="CO">Colombia</option>
+                                                <option value="CR">Costa Rica</option>
+                                                <option value="ES">España</option>
+                                                <option value="US">Estados Unidos</option>
+                                                <option value="MX">México</option>
+                                                <option value="PE">Perú</option>
+                            </Form.Select>
+                            <Form.Text className="text-danger">{errors.pais?.message}</Form.Text>
+                        </Form.Group>
+                        <Button variant="primary" type="submit" className="w-100">
+                          Buscar temperatura
+                        </Button>
+                    </Form>
+                </Card.Body>
+            </Card>
         </section>
     )
 }
